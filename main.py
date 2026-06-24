caa4339dcf6c9dd1b20f139ab62ec69a0923b6ba
unknown <0hoon.jeon@gmail.com>
2026-06-24 19:24:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+with st.sidebar():
+    st.write('hihi')
+
 st.set_page_config(
     page_title="내 앱",
     page_icon="📄",
@@ -9,7 +12,7 @@ st.set_page_config(
 st.title("일반 Streamlit 페이지")
 
 st.header("섹션 제목")
-st.write("여기에 내용을 작성합니다.")
+st.write("여기에 내용을 작.")
 
 name = st.text_input("이름 입력")
 
