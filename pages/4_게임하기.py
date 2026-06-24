2ec49617f981367c65ae096115d3a10288c6c433
unknown <0hoon.jeon@gmail.com>
2026-06-24 21:21:14 +0900

    Update 4_게임하기.py

--- pages/4_게임하기.py
+++ pages/4_게임하기.py
@@ -1,9 +1,3 @@
-import streamlit as st
-
-st.write("cwd:", __import__("os").getcwd())
-st.write("static:", __import__("os").path.exists("static"))
-st.write("tetris:", __import__("os").path.exists("static/tetris.html"))
-
 import streamlit.components.v1 as components
 
 with open("static/tetris.html", encoding="utf-8") as f:
