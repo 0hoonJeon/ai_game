102086c8be025eb4ff8cfc02f38423c662da9b00
unknown <0hoon.jeon@gmail.com>
2026-06-24 21:15:29 +0900

    Create 4_게임하기.py

---
+++ pages/4_게임하기.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <a href="/static/tetris.html" target="_blank">
+        <button>테트리스 실행</button>
+    </a>
+    """,
+    unsafe_allow_html=True
+)
