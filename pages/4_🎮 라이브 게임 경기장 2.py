798159e394bfb5bcc1d4ac5caa9de28e93ec068b
unknown <0hoon.jeon@gmail.com>
2026-06-25 12:40:39 +0900

    Update 4_🎮 라이브 게임 경기장 2.py

--- pages/4_🎮 라이브 게임 경기장 2.py
+++ pages/4_🎮 라이브 게임 경기장 2.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="vampire_survival", layout="wide")
 
-# st.title("👻 vampire_survival")
+st.title("👻 vampire_survival")
 
 # 게임 화면을 불러옵니다
 st.iframe("./static/vampire_survival.html", height=800)
